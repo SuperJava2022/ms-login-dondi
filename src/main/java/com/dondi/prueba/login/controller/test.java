425bc6e16e19d78fbeda2a68621424e49cafcf16
Luis Leon <lleon2319@gmail.com>
2025-03-28 18:57:46 -0500

    ⚡️ Commit Final

--- src/main/java/com/dondi/prueba/login/controller/test.java
+++ src/main/java/com/dondi/prueba/login/controller/test.java
@@ -1,14 +1,33 @@
 package com.dondi.prueba.login.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import com.dondi.prueba.login.entity.Token;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 public class test {
 
     @GetMapping("/prueba")
     public String getMethodName() {
         return new String("Simulo MS Login!!");
+    }
+
+    @PostMapping("/login")
+    public Token postMethodName(@RequestBody String entity) {
+        Token token = new Token();
+        if (entity.equals("Admin")) {
+            token.setToken("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+            return token;
+        }
+        token.setToken("BBCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+        return token;
+
     }
 
 }
---
+++ src/main/java/com/dondi/prueba/login/entity/Token.java
@@ -0,0 +1,18 @@
+package com.dondi.prueba.login.entity;
+
+public class Token {
+    private String token;
+
+    public Token() {
+
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
